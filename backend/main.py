7cc6b05a5a9b71210db3f8029fdde77bf86ad0ea
Islene888 <islenezhao@gmail.com>
2025-07-14 03:40:35 -0700

    docker deployment

--- backend/main.py
+++ backend/main.py
@@ -16,4 +16,4 @@ for rule in app.url_map.iter_rules():
     print(f"Route: {rule}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
